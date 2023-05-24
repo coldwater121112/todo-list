1fd21fcc506bc7e55bd84ded5112687f96095d2d
coldwater121112 <coldwater121112@gmail.com>
2023-05-25 01:25:09 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 
 const exphbs = require('express-handlebars')
+
+const Todo = require('./models/todo')
+
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -24,7 +27,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 app.listen(3000, () => {
